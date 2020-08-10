34bf766b2f67a88bb30f38fc9e73b9f65efb88c7
Calvin Cattaneo <calvin.cattaneo@gmail.com>
2020-08-10 07:21:56 -0300

    projeto

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    private bool ActiveMenu;
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            ActiveMenu = !ActiveMenu;
+            pauseMenu.SetActive(ActiveMenu);
+        }
+    }
+    public void startGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void exitOptionMenu(GameObject go) {
+        go.SetActive(false);
+    }
+
+    public void activeOptionMenu(GameObject go) {
+        go.SetActive(true);
+    }
+
+}
